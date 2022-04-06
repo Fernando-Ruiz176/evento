84517a7c7ff6d95671d6dc983540e62f1b2f3d79
Fernando-Ruiz176 <ruizfe.sud@gmail.com>
2022-04-06 01:29:43 -0400

    Inclusion de nuevos archivos

---
+++ src/AppEvento.java
@@ -0,0 +1,9 @@
+
+public class AppEvento {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/Entrada.java
@@ -0,0 +1,8 @@
+
+public class Entrada {
+	
+	private String codigo;
+	private int asiento;
+	
+
+}
